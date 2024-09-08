8b2eb68d9ebf9b72f3068682758939966a416b62
wuyeelok <wuyeelok@gmail.com>
2024-09-08 14:40:17 +0800

    My 2 pointers solution

--- src/main/java/kenneth/Solution.java
+++ src/main/java/kenneth/Solution.java
@@ -12,7 +12,30 @@ public class Solution {
         // 1 <= word1.length, word2.length <= 100
         // word1 and word2 consist of lowercase English letters
 
-        return "";
+        StringBuilder result = new StringBuilder();
+
+
+        if (word1 != null && word2 != null) {
+            final int word1Len = word1.length();
+            final int word2Len = word2.length();
+
+            int word1Pointer = 0;
+            int word2Pointer = 0;
+
+            while (word1Pointer < word1Len || word2Pointer < word2Len) {
+                if (word1Pointer < word1Len) {
+                    result.append(word1.charAt(word1Pointer));
+                    word1Pointer++;
+                }
+
+                if (word2Pointer < word2Len) {
+                    result.append(word2.charAt(word2Pointer));
+                    word2Pointer++;
+                }
+            }
+        }
+
+        return result.toString();
     }
 
 }
